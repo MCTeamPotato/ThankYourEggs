1a35e5a5e4601364ff3325113eb304db02ba1318
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-10-29 12:02:17 +0800

    1182

--- src/main/java/me/kall/thankyoureggs/ThankYourEggs.java
+++ src/main/java/me/kall/thankyoureggs/ThankYourEggs.java
@@ -1,6 +1,7 @@
 package me.kall.thankyoureggs;
 
 import me.kall.thankyoureggs.config.EggConfig;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -13,8 +14,8 @@ public final class ThankYourEggs {
     public static final String MOD_NAME = "ThankYourEggs";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
-    public ThankYourEggs(FMLJavaModLoadingContext context) {
-        context.registerConfig(ModConfig.Type.COMMON, EggConfig.INSTANCE);
-        context.getModEventBus().addListener(EggConfig::setup);
+    public ThankYourEggs() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, EggConfig.INSTANCE);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(EggConfig::setup);
     }
 }
